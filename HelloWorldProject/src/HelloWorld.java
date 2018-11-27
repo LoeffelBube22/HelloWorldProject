f1b733eeb9d888063796183bc5c2661cf2f5d04d
paul.kreul <paul.kreul@KK-GYM-EDV1-06.gym.kk.local>
2018-11-27 17:39:57 +0100

    kommentar hinzugefügt

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,7 +3,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hallo Klasse");
+		System.out.println("Hallo Klasse");
+// das ist ein kommentar, kein code der ausgeführt wird.
+// das ist ein eizeitiger kommentar
+		/*
+		 * Mehrzeilig Kommentar Schreiben
+		 */
 	}
 
 }
