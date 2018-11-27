6283bcdfc7c1eb55e6571c6165dc89b380687e12
LoeffelBube22 <45207685+LoeffelBube22@users.noreply.github.com>
2018-11-27 17:49:31 +0100

    Update HelloWorld.java

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Klasse");
-// das ist ein kommentar, kein code der ausgeführt wird.
+		System.out.println("Hallo Paul");
+// das ist ein kommentar, kein code der ausgefÃ¼hrt wird.
 // das ist ein eizeitiger kommentar
 		/*
 		 * Mehrzeilig Kommentar Schreiben
