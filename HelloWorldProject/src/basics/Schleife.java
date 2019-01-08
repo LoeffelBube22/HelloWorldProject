33081568173ae89da6f650cb2398ef80ab07d117
paul.kreul <paul.kreul@KK-GYM-EDV1-06.gym.kk.local>
2019-01-08 18:04:19 +0100

    Schleife

---
+++ HelloWorldProject/src/basics/Schleife.java
@@ -0,0 +1,28 @@
+package basics;
+
+public class Schleife {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // For-Schleife - 3 Teilig
+		// 1. variable Init
+		// 2. Bedingung
+		// 3. In/De-Krement
+		for (int i = 0; i < 10; i = i+1) {
+			System.out.println("Zahl ist " + i);
+		}
+		// eine Schleife die von 100-1 zählt
+		// Zahl ist 100
+		// Zahl ist 99
+        // ...
+		// Zahl ist 1
+		for (int i = 100; i > 0;i = i-1) {
+			System.out.println("Zahl ist " + i);
+			
+			
+	
+	
+		}
+	}
+
+}
