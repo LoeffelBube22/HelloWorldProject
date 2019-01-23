02ebe83ee2659420252a60fca4d67f6bd0fc4fb7
paulkreul <paulkreul@10.0.0.120>
2019-01-23 15:55:22 +0100

    Kleines 1mal1

--- HelloWorldProject/src/basics/Kleines1mal1.java
+++ HelloWorldProject/src/basics/Kleines1mal1.java
@@ -4,16 +4,17 @@ public class Kleines1mal1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-for (int i = 1; i <= 10; i++) {
-	System.out.print("Reihe von " + i + ":" +  "\t");
-	for (int e = 1; e <= 10; e++) {
-		System.out.print((e*i) + "\t") ;
-		
-	
-	}
-	System.out.println();
-}
-	
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.print("Reihe von " + i + ":" +  "\t");
+			for (int e = 1; e <= 10; e++) {
+				System.out.print((e*i) + "\t") ;
+				
+			
+			}
+			System.out.println();
 	}
 
 }
+}
+	
